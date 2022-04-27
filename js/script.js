b704c7d7f9b0e9285fea1fdf691f0ab740ff3980
nehru35 <sonehru@gmail.com>
2022-04-27 12:14:48 +0100

    Updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 let btn = document.querySelector(".btn");
 let menu = document.querySelector("#menu");
+let btns = null;
+
+let attr = document.querySelectorAll("empty-link");
+let learn = document.getElementsByClassName("learn-more");
+
+btns.forEach((attr) =>
+  btn.addEventListener('click', noLink)
+);
 
 function showMenu() {
     menu.classList.toggle("show");
 }
 
+function noLink() {
+    window.alert("No link")
+}
+
 btn.addEventListener('click', showMenu);
+attr.addEventListener('click', noLink)
+learn.addEventListener('click', noLink)
