31d43dbe2c65cec172ed68ceb332b4b3077499fe
nehru35 <sonehru@gmail.com>
2022-04-28 19:00:06 +0100

    Added JavaScript function for all empty links

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 let btn = document.querySelector(".btn");
 let menu = document.querySelector("#menu");
-let btns = null;
-
-let attr = document.querySelectorAll("empty-link");
-let learn = document.getElementsByClassName("learn-more");
 
 function showMenu() {
     menu.classList.toggle("show");
@@ -14,5 +10,28 @@ function noLink() {
 }
 
 btn.addEventListener('click', showMenu);
-attr.addEventListener('click', noLink)
-learn.addEventListener('click', noLink)
+
+
+// Melhorar o codigo abaixo
+
+let msg1 = document.getElementById('link1');
+let msg2 = document.getElementById('link2');
+let msg3 = document.getElementById('link3');
+let msg4 = document.getElementById('link4');
+
+let more1 = document.querySelector('#more1');
+let more2 = document.querySelector('#more2');
+let more3 = document.querySelector('#more3');
+let more4 = document.querySelector('#more4');
+let more5 = document.querySelector('#more5');
+
+link1.addEventListener('click', noLink);
+link2.addEventListener('click', noLink);
+link3.addEventListener('click', noLink);
+link4.addEventListener('click', noLink);
+
+more1.addEventListener('click', noLink);
+more2.addEventListener('click', noLink);
+more3.addEventListener('click', noLink);
+more4.addEventListener('click', noLink);
+more5.addEventListener('click', noLink);
