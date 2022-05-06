de96c18192501d38550ad3068d5676eb2c648caf
nehru35 <sonehru@gmail.com>
2022-05-06 05:27:32 +0100

    Updated the header

--- js/script.js
+++ js/script.js
@@ -14,21 +14,11 @@ btn.addEventListener('click', showMenu);
 
 // Melhorar o codigo abaixo
 
-let msg1 = document.getElementById('link1');
-let msg2 = document.getElementById('link2');
-let msg3 = document.getElementById('link3');
-let msg4 = document.getElementById('link4');
-
 let more1 = document.querySelector('#more1');
 let more2 = document.querySelector('#more2');
 let more3 = document.querySelector('#more3');
 let more4 = document.querySelector('#more4');
 let more5 = document.querySelector('#more5');
-
-link1.addEventListener('click', noLink);
-link2.addEventListener('click', noLink);
-link3.addEventListener('click', noLink);
-link4.addEventListener('click', noLink);
 
 more1.addEventListener('click', noLink);
 more2.addEventListener('click', noLink);
